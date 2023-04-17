9faba97afee0527c2322cb01d831b801de107fbe
cyberfolk <andrea1995ferrari.com>
2023-04-17 22:46:22 +0200

    Remove v-for and v-in in the same statement

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -16,11 +16,13 @@ export default {
 
 <template>
   <div class="container p-5">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
-      <CardItem class="col mt-4" v-for="card in store.cards" :card="card" v-if="!store.loading" />
-      <div class="loader" v-else>
-        <p>loading...</p>
+    <div v-if="!store.loading_cards">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
+        <CardItem class="col mt-4" v-for="card in store.cards" :card="card" />
       </div>
+    </div>
+    <div class="loader" v-else>
+      <p>loading...</p>
     </div>
   </div>
 </template>
