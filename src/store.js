926af6bd079b4fed26a946a041f469a4be466e80
cyberfolk <andrea1995ferrari.com>
2023-04-17 15:17:04 +0200

    Change url and name object container

--- src/store.js
+++ src/store.js
@@ -4,15 +4,15 @@ import axios from "axios";
 export const store = reactive({
     searchText: "",
     loading: true,
-    API_URL: "https://rickandmortyapi.com/api/character",
-    characters: null,
+    API_URL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+    cards: null,
     info: null,
     fetchCard(url) {
         axios
             .get(url)
             .then((response) => {
                 //console.log(response.data);
-                this.characters = response.data.results;
+                this.cards = response.data.results;
                 this.info = response.data.info;
                 this.loading = false;
             })
