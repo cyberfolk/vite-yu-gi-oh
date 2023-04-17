2d7cb4e19aa76ac6f110a67db8bf62edcd845a4a
cyberfolk <andrea1995ferrari.com>
2023-04-17 15:15:26 +0200

    Add logo in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
   <header>
-    <h1 class="text-center py-5">Yu-Gi-Oh API</h1>
+    <div class="d-flex">
+      <img class="align-self-center px-3" height="25" src="../assets/img/logo.jpeg" alt="" />
+      <h1 class="py-1">Yu-Gi-Oh API</h1>
+    </div>
   </header>
 </template>
