84f69e844bc8c86eb8f60d6cb471fcd249de4d66
cyberfolk <andrea1995ferrari.com>
2023-04-17 22:46:54 +0200

    Implement 2 different loading

--- src/store.js
+++ src/store.js
@@ -2,21 +2,18 @@ import { reactive } from 'vue'
 import axios from "axios";
 
 export const store = reactive({
-    searchText: "",
-    loading: true,
+    loading_cards: true,
+    loading_archetypes: true,
     API_URL_CARD: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=1000&offset=0",
     API_URL_ARCHETYPE: "https://db.ygoprodeck.com/api/v7/archetypes.php",
     cards: null,
-    info: null,
     archetypes: null,
     fetchCards(url) {
         axios
             .get(url)
             .then((response) => {
                 this.cards = response.data.data;
-                this.loading = false;
-                //console.log(response)
-                //console.log(target[this.cards])
+                this.loading_cards = false;
             })
             .catch((err) => {
                 console.log(err);
@@ -28,8 +25,7 @@ export const store = reactive({
             .get(url)
             .then((response) => {
                 this.archetypes = response.data;
-                //console.log(response)
-                //console.log(target[this.cards])
+                this.loading_archetypes = false;
             })
             .catch((err) => {
                 console.log(err);
