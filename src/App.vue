7c65b0520044b74c130efb24502b771b197e2fc7
cyberfolk <andrea1995ferrari.com>
2023-04-17 14:35:09 +0200

    Insert basic component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
-<script></script>
+<script>
+import { store } from "./store.js";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
 
-<template></template>
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {},
+  mounted() {
+    store.fetchCard(this.store.API_URL);
+  },
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+</template>
 
 <style></style>
