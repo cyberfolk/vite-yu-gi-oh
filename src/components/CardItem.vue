1a905339f38acb0c1587ff732d6c148debc65a1d
cyberfolk <andrea1995ferrari.com>
2023-04-18 18:09:30 +0200

    Crop border card and apply transition scale

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -9,8 +9,8 @@ export default {
 
 <template>
   <div>
-    <div class="ycard">
-      <img class="img-fluid" :src="card.card_images[0].image_url" :alt="card.name" />
+    <div class="card border border-light shadow-sm">
+      <img class="card-img-top" :src="card.card_images[0].image_url" :alt="card.name" />
       <div class="text text-center info pt-3 px-3">
         <div class="text-light name text-uppercase pb-3">{{ card.name }}</div>
         <div class="text-light name text-dark pb-3">{{ card.archetype }}</div>
@@ -22,8 +22,13 @@ export default {
 <style lang="scss" scoped>
 @use "../assets/scss/partials/variables" as *;
 
-.ycard {
+.card {
   height: 100%;
   background-color: $primary;
+  transition: transform 0.5s;
+}
+
+.card:hover {
+  transform: scale(1.1);
 }
 </style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <template>
-  <section class="p-5">
+  <section class="p-5 rounded">
     <PulseLoader v-if="store.loading_cards" color="#d48f38" size="30px"></PulseLoader>
     <div v-else class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
       <CardItem class="col mt-4" v-for="card in store.cards" :card="card" />
